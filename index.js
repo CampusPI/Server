783d617cbad456e0a4a7b4f12e471361c970277b
pefido <pedrofilipeduraes@hotmail.com>
2014-05-08 14:42:16 +0100

    routes para api da tv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var request = require('request');
 var Hapi = require("hapi");
 var server = new Hapi.Server(8080, "localhost");
 
@@ -14,5 +15,72 @@ server.route({
     reply({"response": "ok"});
   }
 });
+
+
+server.route({path: "/API/TV/Weather", method: "GET", config: {handler: getWeather}});
+server.route({path: "/API/TV/Broadcast", method: "GET", config: {handler: getBroadcastList}});
+server.route({path: "/API/TV/Content", method: "GET", config: {handler: getContentList}});
+server.route({path: "/API/TV/Video", method: "GET", config: {handler: getVideoList}});
+
+
+function requestWeather(){
+	request('http://api.wunderground.com/api/8375472c04b107a7/conditions/q/Portugal/Montijo.json', 'json', function(error, response, body){
+		if (!error && response.statusCode == 200){
+    			 return body;
+  			}
+	});
+}
+
+function getWeather(request, reply){
+	var weather = requestWeather();
+	reply(weather);
+}
+
+//substituir por informacao a serio
+function getBroadcastList(request, reply){
+	var broadcastList =[{
+		priority: 2,
+		type: "informacao",
+		text: "Este texto representa a informacao a passar em rodape a girar toda bonita"
+	}]
+
+	reply(broadcastList);
+}
+
+function getContentList(request, reply){
+	var contentList =[{
+		type: "Transportes",
+		obj: "",
+		title: "Fertagus",
+		content: [
+			["Roma-Areeiro", "15:10"],
+			["Roma-Areeiro", "15:30"],
+			["Setubal", "16:00"],
+			["Coina", "16:20"]
+		]
+	}]
+
+	reply(contentList);
+}
+
+function getVideoList(request, reply){
+	var videoList=[{
+			link: "www.youtube.com/vid1",
+			title: "Coelho1",
+			hour: "15:30",
+			length: "2:50",
+			description: "coelho aos saltos muito rapido"
+		},
+		{
+			link: "www.youtube.com/vid2",
+			title: "Coelho2",
+			hour: "15:30",
+			length: "2:50",
+			description: "coelho aos saltos muito rapido"
+		}
+	]
+
+	reply(videoList);
+}
 
 module.exports = server;

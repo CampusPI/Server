885d17f390bbf2c8982283923da09b8c16653136
João Campinhos <joao.campinhos@gmail.com>
2014-05-10 17:53:44 +0100

    fix server

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ server.route({
 
 
 server.route({path: "/api/tv/weather", method: "GET", config: {handler: getWeather}});
-server.route({path: "/api/tv/broadcast", method: "GET", config: {handler: getBroadcastList}});
-server.route({path: "/api/tv/content", method: "GET", config: {handler: getContentList}});
-server.route({path: "/api/tv/video", method: "GET", config: {handler: getVideoList}});
+server.route({path: "/api/tv/broadcast", method: "GET", config: {handler: getBroadcast}});
+server.route({path: "/api/tv/content", method: "GET", config: {handler: getContent}});
+server.route({path: "/api/tv/video", method: "GET", config: {handler: getVideo}});
 
 
 function getWeather(request, reply){
-  req('http://api.wunderground.com/api/8375472c04b107a7/conditions/q/Portugal/Montijo.json', function(error, response, body){
+  req('http://api.wunderground.com/api/8375472c04b107a7/conditions/q/Portugal/Almada.json', function(error, response, body){
     if (!error && response.statusCode == 200){
       body = JSON.parse(body);
       reply({
@@ -35,18 +35,24 @@ function getWeather(request, reply){
   });
 }
 
-//substituir por informacao a serio
-function getBroadcastList(request, reply){
-  var broadcastList =[{
-    priority: 2,
-    type: "informacao",
-    text: "Este texto representa a informacao a passar em rodape a girar toda bonita"
-  }];
+function getBroadcast(request, reply){
+  var broadcastList = [
+    {
+      priority: 2,
+      type: 'Importante',
+      text: 'Este é o primeiro aviso que avisa aos que foram avisados previamente que se pretenderem continuar a ser avisados terão de nos avisar.'
+    },
+    {
+      priority: 1,
+      type: 'Greve',
+      text: 'Este é o segundo aviso que cenas fixes.'
+    }
+  ];
 
   reply(broadcastList);
 }
 
-function getContentList(request, reply){
+function getContent(request, reply){
   var contentList =[{
     type: "Transportes",
     obj: "",
@@ -62,21 +68,22 @@ function getContentList(request, reply){
   reply(contentList);
 }
 
-function getVideoList(request, reply){
-  var videoList=[{
-    link: "www.youtube.com/vid1",
-    title: "Coelho1",
-    hour: "15:30",
-    length: "2:50",
-    description: "coelho aos saltos muito rapido"
-  },
-  {
-    link: "www.youtube.com/vid2",
-    title: "Coelho2",
-    hour: "15:30",
-    length: "2:50",
-    description: "coelho aos saltos muito rapido"
-  }
+function getVideo(request, reply){
+  var videoList = [
+    {
+      link: "www.youtube.com/vid1",
+      title: "Coelho1",
+      hour: "15:30",
+      length: "2:50",
+      description: "coelho aos saltos muito rapido"
+    },
+    {
+      link: "www.youtube.com/vid2",
+      title: "Coelho2",
+      hour: "15:30",
+      length: "2:50",
+      description: "coelho aos saltos muito rapido"
+    }
   ];
 
   reply(videoList);

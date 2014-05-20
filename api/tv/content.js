3c409ac99d2fa3113c9227ca5abba02e909a0d02
pefido <pedrofilipeduraes@hotmail.com>
2014-05-20 18:53:54 +0100

    limpar

--- api/tv/content.js
+++ api/tv/content.js
@@ -1,16 +1,4 @@
 var getContent = function (request, reply){
-  /*var contentList =[
-    {
-      type: 'Transportes',
-      title: 'Fertagus',
-      content: [
-        ['Roma-Areeiro', '15:10'],
-        ['Roma-Areeiro', '15:30'],
-        ['Setubal', '16:00'],
-        ['Coina', '16:20']
-      ]
-    }
-  ];*/
 
     var db = request.server.plugins['hapi-mongodb'].db;
 

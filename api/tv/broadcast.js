3fdab1c3626daf2f1c2158c03b19e8e5ac9b6ed4
pefido <pedrofilipeduraes@hotmail.com>
2014-05-20 15:26:44 +0100

    get broadcast from db

--- api/tv/broadcast.js
+++ api/tv/broadcast.js
@@ -1,18 +1,9 @@
 var getBroadcast = function (request, reply){
-  var broadcastList = [
-    {
-      priority: 2,
-      type: 'Importante',
-      text: 'Este é o primeiro aviso que avisa aos que foram avisados previamente que se pretenderem continuar a ser avisados terão de nos avisar.'
-    },
-    {
-      priority: 1,
-      type: 'Greve',
-      text: 'Este é o segundo aviso que cenas fixes.'
-    }
-  ];
+    var db = request.server.plugins['hapi-mongodb'].db;
 
-  reply(broadcastList);
+    db.collection('broadcasts').find().toArray(function(err, results){
+        reply(results);
+    })
 };
 
 module.exports = getBroadcast;

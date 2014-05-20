4b4313c2c5b9b812488db3ef57c571937099470c
pefido <pedrofilipeduraes@hotmail.com>
2014-05-20 21:02:46 +0100

    merge

--- api/tv/content.js
+++ api/tv/content.js
@@ -1,19 +1,9 @@
 var getContent = function (request, reply){
-<<<<<<< HEAD
-
     var db = request.server.plugins['hapi-mongodb'].db;
 
     db.collection('contents').find().toArray(function(err, results){
         reply(results);
     });
-};
-
-module.exports = getContent;
-=======
-  var db = request.server.plugins['hapi-mongodb'].db;
-  db.collection('content').find().toArray(function(err, items) {
-    reply(items[0]);
-  });
 };
 
 module.exports = getContent;
@@ -27,4 +17,3 @@ module.exports = getContent;
 // {"type": "Ementa", "title": "Teresa Gato", "food": "Sopa", "plate": "Legumes"}
 // {"type": "Ementa", "title": "Teresa Gato", "food": "Peixe", "plate": "Maruca Assada"}
 // {"type": "Ementa", "title": "Teresa Gato", "food": "Carne", "plate": "Bitoque"}
->>>>>>> 939d36342d887fb08619c960f70e8ebe9de794c2

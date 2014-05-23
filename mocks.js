179c83fa6b757be21f0ef511d4e83479b629996e
João Campinhos <joao.campinhos@gmail.com>
2014-05-23 16:24:04 +0100

    dummy data. broadcasts

---
+++ mocks.js
@@ -0,0 +1,52 @@
+var mongojs = require('mongojs');
+var db = mongojs(require('./config/db').url);
+var args = process.argv.slice(2);
+
+switch(args[0]) {
+case 'broadcast':
+  dbBroadcast();
+  break;
+case 'content':
+  dbContent();
+  break;
+case 'delete':
+  dbDelete();
+  break;
+default:
+  dbBroadcast();
+  dbContent();
+  console.log('all');
+}
+
+function dbBroadcast() {
+
+  var o = [
+    {
+      priority: 1,
+      type: 'Greve',
+      text: 'MTS - Greve às horas extraordinárias durante o dia 2 de Junho das 16 às 22 horas.'
+    },
+    {
+      priority: 1,
+      type: 'Importante',
+      text: 'Dia 2 de Junho o Parque do DI estará encerrado para manutenção. Pedimos desculpa pelo incómodo.'
+    }
+  ];
+
+  insert('broadcasts',o);
+}
+
+function dbContent() {
+  console.log('content');
+}
+
+function dbDelete() {
+  console.log('delete');
+}
+
+function insert(col,o) {
+  db.collection(col).remove({ '_id' : { $exists : true } }, function() {
+    db.collection(col).insert(o);
+    db.close();
+  });
+}

fe9daf0a5046373a6bd11f3ef21bc112ebf2be3e
pefido <pedrofilipeduraes@hotmail.com>
2014-06-17 16:42:37 +0100

    adicionar route para as news

---
+++ api/tv/news.js
@@ -0,0 +1,9 @@
+var getVideos = function (request, reply){
+  var db = request.server.plugins['hapi-mongodb'].db;
+
+  db.collection('news').find().toArray(function(err, results){
+    reply(results);
+  });
+};
+
+module.exports.handler = getVideos;

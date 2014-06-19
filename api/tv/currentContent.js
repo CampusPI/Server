47b864e09207ff522a37d68e6ef79ed0cda853f0
pefido <pedrofilipeduraes@hotmail.com>
2014-06-19 19:36:40 +0100

    currentContent

--- api/tv/currentContent.js
+++ api/tv/currentContent.js
@@ -1,15 +1,27 @@
-var setCurrentContent = function (request, reply){
+var currentContent = function(request, reply) {
+  //var db = request.server.plugins.mongodb.db;
   var db = request.server.plugins.mongodb.db;
   var currentContent = db.collection('currentContent');
+  switch (request.method) {
+    case 'get':
+      currentContent.find().toArray(function (err, results) {
+        reply(results);
+      });
+      break;
+    case 'post':
+      currentContent.remove({}, function () {
+          currentContent.insert(request.payload[0], function(){});
+      });
 
-  currentContent.remove({}, function(){
-    currentContent.insert(request.payload, function() {});
-  });
-
-  reply({
-    error: null,
-    success: true
-  });
+      reply({
+        error: null,
+        success: true
+      });
+      break;
+    default:
+      //erro
+      break;
+  }
 };
 
-module.exports.handler = setCurrentContent;
+module.exports.handler = currentContent;

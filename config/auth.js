ba474b22fa87579da20c99b799932072a8d59a16
João Campinhos <joao.campinhos@gmail.com>
2014-06-25 13:08:53 +0100

    hack para funcionar no mobile sem perdermos tempo com a auth

--- config/auth.js
+++ config/auth.js
@@ -6,35 +6,46 @@ var get = require('superagent').get;
 module.exports = function(server) {
 
   var validateFunc = function (token, callback) {
-    //Confirmar se o token é válido
-    get('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token, function(res){
-      if (res.body.issued_to !== id) {
-        return callback('nope',null);
-      }
-      //Confirmar hd
-      get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+token, function(res){
-        if (res.body.hd !== hd) {
+    //FAKE IT TILL YOU MAKE IT!
+    if (token === '109029607727800353127') {
+      server.plugins.mongodb.db.collection('users').find({id: token}).toArray(function(err, results){
+        var user = results[0];
+        user.token = token;
+        callback(null,results[0]);
+      });
+    }
+    else {
+      //Confirmar se o token é válido
+      get('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token, function(res){
+        if (res.body.issued_to !== id) {
           return callback('nope',null);
         }
-        //Encontrar o user que corresponde ao ID do token
-        var db = server.plugins.mongodb.db;
+        //Confirmar hd
+        get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+token, function(res){
+          if (res.body.hd !== hd) {
+            return callback('nope',null);
+          }
+          //Encontrar o user que corresponde ao ID do token
+          var db = server.plugins.mongodb.db;
 
-        db.collection('users').find({id: res.body.id}).toArray(function(err, results){
-          var user;
-          if (results.length === 0) {
-            //Não existe? não faz mal vamos criar
-            user = res.body;
-            db.collection('users').insert(user, function(err) {if (!err) {console.log('[User] New User');}});
-          }
-          else {
-            //Existe? óptimo!
-            user = results[0];
-          }
-          user.token = token;
-          callback(null,user);
+          db.collection('users').find({id: res.body.id}).toArray(function(err, results){
+            var user;
+            if (results.length === 0) {
+              //Não existe? não faz mal vamos criar
+              user = res.body;
+              db.collection('users').insert(user, function(err) {if (!err) {console.log('[User] New User');}});
+            }
+            else {
+              //Existe? óptimo!
+              user = results[0];
+            }
+            user.token = token;
+            callback(null,user);
+          });
         });
       });
-    });
+    }
+
   };
 
   server.auth.strategy('bearer', 'bearer', { validateFunc: validateFunc });

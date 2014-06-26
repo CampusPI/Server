bc99e8694f0fffa66706beb671424653a439607a
jpgpereira <jpgpereira.92@gmail.com>
2014-06-26 11:45:46 +0100

    cenas cheats

--- api/dev/cheats.js
+++ api/dev/cheats.js
@@ -2,37 +2,32 @@ var setSchedule = function (request, reply){
   var db = request.server.plugins.mongodb.db;
   
   var res = [];
-//p0eLp-9RFK4
 
   db.collection('videos').find({videoId: 'p0eLp-9RFK4'}).toArray(function(err, results){
+    results[0].type = 'video';
     res[0] = results[0];
 
-
   db.collection('schedule').find({titulo: 'Lançamento Livro'}).toArray(function(err, results){
+    results[0].type = 'biblio';
     res[1] = results[0];
 
-
   db.collection('schedule').find({titulo: 'Cineclube | Imitation of Life'}).toArray(function(err, results){
+    results[0].type = 'biblio';
     res[2] = results[0];
 
   db.collection('schedule').find({titulo: 'Ciclo Master of Glass Art and Science'}).toArray(function(err, results){
+    results[0].type = 'biblio';
     res[3] = results[0];
 
   reply(res);
 
   });
 
-
-
   });
 
   });
 
-
   });
-
-
-
 
 };
 

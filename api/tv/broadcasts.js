26877cf802ed7c767321247a45f0b92ad1631278
jpgpereira <jpgpereira.92@gmail.com>
2014-07-01 18:35:42 +0100

    adicionar broadcasts

---
+++ api/tv/broadcasts.js
@@ -0,0 +1,9 @@
+var getBroadcasts = function (request, reply){
+    var db = request.server.plugins.mongodb.db;
+
+    db.collection('broadcasts').find().toArray(function(err, results){
+        reply(results);
+    });
+};
+
+module.exports.handler = getBroadcasts;

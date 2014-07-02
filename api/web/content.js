3c93bbc20e3e263633b42e35ca89a68956c4b2a7
joaocampinhos <joao.campinhos@gmail.com>
2014-07-02 16:54:57 +0100

    finito

--- api/web/content.js
+++ api/web/content.js
@@ -3,17 +3,14 @@ var getContent = function (request, reply){
   var ob = request.server.plugins.mongodb.ObjectID;
   var x = ob(request.params.id);
   db.collection('videos').findOne({'_id': x},function(err, results){
-    console.log(results);
     if (results) {
       reply(results);
     }
     db.collection('biblio').findOne({'_id': x},function(err, result){
-      console.log(result);
       if (result) {
         reply(result);
       }
       db.collection('news').findOne({'_id': x},function(err, resul){
-        console.log(resul);
         if (resul) {
           reply(resul);
         }

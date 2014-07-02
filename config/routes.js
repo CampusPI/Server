7fc2670f61a8f219fffccca7e7b4945cba9b549b
pefido <pedrofilipeduraes@hotmail.com>
2014-07-02 16:42:03 +0100

    add route

--- config/routes.js
+++ config/routes.js
@@ -38,6 +38,7 @@ routes.push({path: '/api/web/videos', method: 'GET', config: controller.web.vide
 routes.push({path: '/api/web/video/{id}', method: 'GET', config: controller.web.video});
 routes.push({path: '/api/web/favorites', method: 'GET', config: controller.web.fav.get});
 routes.push({path: '/api/web/favorites', method: 'POST', config: controller.web.fav.post});
+routes.push({path: '/api/web/favorites', method: 'DELETE', config: controller.web.fav.delete});
 
 //Retornar user logado
 routes.push({path: '/api/web/user', method: 'GET', config: controller.web.user});

a1d45aa6edfc2247a7431c33efa4bfab271418f7
jpgpereira <jpgpereira.92@gmail.com>
2014-07-02 12:32:41 +0100

    broadcasts mais strikes

--- api/tv/broadcasts.js
+++ api/tv/broadcasts.js
@@ -1,8 +1,20 @@
 var getBroadcasts = function (request, reply){
     var db = request.server.plugins.mongodb.db;
 
+    var res = [];
+
     db.collection('broadcasts').find().toArray(function(err, results){
-        reply(results);
+      for (var r in results){
+        res.push(results[r]);
+      }
+      db.collection('strikes').find().toArray(function(err, results){
+        for (var r in results){
+          res.push(results[r]);
+        }
+        if(!err){
+          reply(res);
+        }
+      });
     });
 };
 

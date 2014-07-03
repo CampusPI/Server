4b70c1fe644b8a4fe191f922debc124da6eaa6b6
joaocampinhos <joao.campinhos@gmail.com>
2014-07-03 23:12:15 +0100

    tentativa de sort

--- api/tv/lastContent.js
+++ api/tv/lastContent.js
@@ -1,20 +1,19 @@
-var i = 0;
 
 var lastContent = function(request, reply) {
   var db = request.server.plugins.mongodb.db;
   var lastContent = db.collection('lastContent');
   switch (request.method) {
-    case 'get':
-      lastContent.find().toArray(function (err, results) {
-        reply(results);
-      });
-      break;
-    default:
-      reply({
-        error: 'invalid operation',
-        success: false
-      });
-      break;
+  case 'get':
+    lastContent.find().sort({count: -1}, function (err, results) {
+      reply(results);
+    });
+    break;
+  default:
+    reply({
+      error: 'invalid operation',
+      success: false
+    });
+    break;
   }
 };
 
